f602158dcfa19d2a79cfb9beeafd896498e34b04
David C <david@thecallenders.com>
2025-06-20 14:50:03 -0500

    responsive design fix

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -7,8 +7,8 @@ import { Button } from "@components/Button";
 
 const ResultsRoot: FC = () => {
 	return (
-		<div className="m-4 flex flex-col items-start gap-4">
-			<Link href="/">
+		<div className="m-4 flex flex-col gap-4">
+			<Link href="/" className="w-fit">
 				<Button>&larr; Back</Button>
 			</Link>
 			<Suspense fallback={<div>Loading</div>}>
