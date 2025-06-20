da7a0e2b99ab718254bfed174e257659f9ede228
David C <david@thecallenders.com>
2025-06-20 13:59:48 -0500

    copy ux

--- src/app/results/_components/CopyToClipboard.tsx
+++ src/app/results/_components/CopyToClipboard.tsx
@@ -1,4 +1,4 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 
 import { Button } from "@components/Button";
 
@@ -6,9 +6,48 @@ type Props = {
 	text: string;
 };
 
+const statusColor = (status: string): string => {
+	switch (status) {
+		case "good": {
+			return "bg-green-100";
+		}
+		case "bad": {
+			return "bg-red-100";
+		}
+		case "inprogress": {
+			return "bg-yellow-100";
+		}
+		default: {
+			return "";
+		}
+	}
+};
+const copyTextToClipboard = async (text: string): Promise<void> => {
+	await navigator.clipboard.writeText(text);
+};
+
 export const CopyToClipboard: FC<Props> = ({ text }) => {
+	const [status, setStatus] = useState<"none" | "good" | "bad" | "inprogress">(
+		"none",
+	);
+	const handleCopy = (): void => {
+		setStatus("inprogress");
+		copyTextToClipboard(text)
+			.then(() => {
+				setStatus("good");
+				setTimeout(() => {
+					setStatus("none");
+				}, 1000);
+			})
+			.catch(() => {
+				setStatus("bad");
+				setTimeout(() => {
+					setStatus("none");
+				}, 1000);
+			});
+	};
 	return (
-		<Button onClick={() => void navigator.clipboard.writeText(text)}>
+		<Button className={statusColor(status)} onClick={handleCopy}>
 			Copy Results
 		</Button>
 	);
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ export const Button: FC<Props> = ({ children, className, ...rest }) => {
 		<button
 			{...rest}
 			className={
-				"bg-gray-100 border border-gray-300 hover:bg-gray-200 rounded-lg px-4 py-2 hover:cursor-pointer w-fit" +
+				"bg-gray-100 border border-gray-300 hover:bg-gray-200 rounded-lg px-4 py-2 hover:cursor-pointer w-fit " +
 				(className ?? "")
 			}
 		>
