5e031e5402d4a93e6f8157facfffedb5440edcfb
David C <david@thecallenders.com>
2025-07-09 14:48:48 -0500

    switch to bun and update packages

--- src/app/results/_components/ResultsView/ResultsView.tsx
+++ src/app/results/_components/ResultsView/ResultsView.tsx
@@ -10,24 +10,22 @@ type Props = {
 
 export const ResultsView: FC<Props> = ({ results }) => {
 	return (
-		<div className="w-full snap-x snap-mandatory overflow-x-auto">
-			<div className="flex w-full flex-row gap-8">
-				<SectionView sectionResults={results.uptake}>
-					Total Uptake<sup>1</sup>
-					<br />
-					(lbs/a)
-				</SectionView>
-				<SectionView sectionResults={results.removal}>
-					Total Removal in Grain
-					<br />
-					(lbs/a)
-				</SectionView>
-				<SectionView sectionResults={results.stover}>
-					Total Removal in Stover
-					<br />
-					(lbs/ton)
-				</SectionView>
-			</div>
+		<div className="flex w-full snap-x snap-mandatory flex-row gap-8 overflow-x-auto">
+			<SectionView sectionResults={results.uptake}>
+				Total Uptake<sup>1</sup>
+				<br />
+				(lbs/a)
+			</SectionView>
+			<SectionView sectionResults={results.removal}>
+				Total Removal in Grain
+				<br />
+				(lbs/a)
+			</SectionView>
+			<SectionView sectionResults={results.stover}>
+				Total Removal in Stover
+				<br />
+				(lbs/ton)
+			</SectionView>
 		</div>
 	);
 };
